9bbd17c62506828035c5377490f08d079f92be8e
hauszmanndavid <hauszmannd@gmail.com>
2026-02-03 13:15:31 +0100

    asd

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,25 +1,13 @@
 import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  onSubmit(formData: NgForm) {
-    if (!formData.valid) {
-      return;
-    }
-
-
-    const enteredEmail = formData.form.value.email;
-    const enteredPassword = formData.form.value.password;
-
-    console.log(formData.form);
-
-    console.log(enteredEmail, enteredPassword);
-  }
+  
 }
